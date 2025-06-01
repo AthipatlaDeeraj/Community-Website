547250a6cb1968b39bffd0f46851b91fd6093d97
AthipatlaDeeraj <deeruathipatla@gmail.com>
2025-06-01 16:12:14 +0530

    change1

--- models/mongo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String
-});
-
-const User = mongoose.model("User", userSchema);
-export default User;
